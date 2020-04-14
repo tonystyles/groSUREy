e9c33d44291c385eb92d0e6d0e7a3ae5fa5da035
tonyito <tonyitocole@gmail.com>
2020-04-14 11:22:39 -0700

    Server set up

---
+++ server/server.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+import { Application } from 'express'
+
+// Create a new express app instance
+const app: Application = express();
+
+app.get('/', function (req, res) {
+
+res.send('Hello World?');
+});
+app.listen(3001, function () {
+console.log('App is listening on port 3001!');
+});
