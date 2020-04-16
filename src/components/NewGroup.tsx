d48b6a821a72e963efe48cbf001db24458a2f135
andieritter <58995450+andieritter@users.noreply.github.com>
2020-04-16 16:42:40 -0700

    made new group component form for user to create new group

--- src/components/NewGroup.tsx
+++ src/components/NewGroup.tsx
@@ -1,13 +1,19 @@
 import * as React from 'react';
 import { TextField } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
 
 const NewGroup: React.FC = (): JSX.Element => {
 
   return (
-    <div>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       <form>
         <TextField id="group-name" label="group name" variant="outlined"></TextField>
-        <TextField id="add-member" label="member" variant="outlined"></TextField>
+        <br />
+        <TextField id="add-member" label="add members" variant="outlined"></TextField>
+        <br />
+        <Button size="large" className="submtiBtn" >
+          Submit
+        </Button>
       </form>
     </div>
   )
