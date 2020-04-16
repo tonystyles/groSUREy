8056c9a165e181401e6e99a3283f35474931d584
andieritter <58995450+andieritter@users.noreply.github.com>
2020-04-16 16:41:07 -0700

    cleaned up new item form

--- src/components/newItem.tsx
+++ src/components/newItem.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
 import { TextField } from '@material-ui/core';
 
 const NewItem: React.FC = (): JSX.Element => {
