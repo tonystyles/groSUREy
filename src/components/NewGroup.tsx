82b795219983d2656aaeb13d4712c2a95667fbc4
andieritter <58995450+andieritter@users.noreply.github.com>
2020-04-16 11:31:43 -0700

    created new group form component

--- src/components/NewGroup.tsx
+++ src/components/NewGroup.tsx
@@ -6,7 +6,8 @@ const NewGroup: React.FC = (): JSX.Element => {
   return (
     <div>
       <form>
-
+        <TextField id="group-name" label="group name" variant="outlined"></TextField>
+        <TextField id="add-member" label="member" variant="outlined"></TextField>
       </form>
     </div>
   )
